529ab439ce450725acdbbbb60ade7e78fecbde49
Mihai Butnariu <mihai.alex.butnariu@gmail.com>
2026-03-07 14:31:12 +0100

    12.4

--- 09-starting-project/src/app/auth/login/login.component.ts
+++ 09-starting-project/src/app/auth/login/login.component.ts
@@ -1,22 +1,83 @@
 import { Component } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  onSubmit(formData: NgForm) {
-    if (formData.form.invalid) {
-      return;
-    }
-    const enteredEmail = formData.form.value.email;
-    const enteredPassword = formData.form.value.password;
-
-    console.log(formData.form);
-    console.log(enteredEmail, enteredPassword);
-  }
+  form = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
 }
+
+
+
+// TEMPLATE FORMS
+// ---------------------
+// import {
+//   afterNextRender,
+//   Component,
+//   DestroyRef,
+//   inject,
+//   viewChild,
+// } from '@angular/core';
+// import { FormsModule, NgForm } from '@angular/forms';
+// import { debounceTime } from 'rxjs';
+
+// @Component({
+//   selector: 'app-login',
+//   standalone: true,
+//   imports: [FormsModule],
+//   templateUrl: './login.component.html',
+//   styleUrl: './login.component.css',
+// })
+// export class LoginComponent {
+//   private form = viewChild.required<NgForm>('form');
+//   private destroyRef = inject(DestroyRef);
+
+//   constructor() {
+//     afterNextRender(() => {
+//       const savedForm = window.localStorage.getItem('saved-login-form');
+
+//       if (savedForm) {
+//         const loadedFormData = JSON.parse(savedForm);
+//         const savedEmail = loadedFormData.email;
+//         setTimeout(() => {
+//           this.form().controls['email'].setValue({
+//             email: savedEmail,
+//           });
+//         }, 1);
+//       }
+
+//       const subscription = this.form()
+//         .valueChanges?.pipe(debounceTime(500))
+//         .subscribe({
+//           next: (value) =>
+//             window.localStorage.setItem(
+//               'saved-login-form',
+//               JSON.stringify({ email: value.email }),
+//             ),
+//         });
+
+//       this.destroyRef.onDestroy(() => subscription?.unsubscribe());
+//     });
+//   }
+
+//   onSubmit(formData: NgForm) {
+//     if (formData.form.invalid) {
+//       return;
+//     }
+
+//     const enteredEmail = formData.form.value.email;
+//     const enteredPassword = formData.form.value.password;
+
+//     console.log(formData.form);
+//     console.log(enteredEmail, enteredPassword);
+
+//     formData.form.reset();
+//   }
+// }
