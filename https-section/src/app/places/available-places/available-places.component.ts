f3492e3d833421e22538425871aeab5a0bacd07b
Mihai Butnariu <mihai.alex.butnariu@gmail.com>
2026-03-10 19:37:15 +0200

    http section start

---
+++ https-section/src/app/places/available-places/available-places.component.ts
@@ -0,0 +1,37 @@
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+
+import { Place } from '../place.model';
+import { PlacesComponent } from '../places.component';
+import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { HttpClient } from '@angular/common/http';
+import { subscribeOn } from 'rxjs';
+
+@Component({
+  selector: 'app-available-places',
+  standalone: true,
+  templateUrl: './available-places.component.html',
+  styleUrl: './available-places.component.css',
+  imports: [PlacesComponent, PlacesContainerComponent],
+})
+export class AvailablePlacesComponent implements OnInit {
+  places = signal<Place[] | undefined>(undefined);
+  private httpClient = inject(HttpClient);
+  private destroyDef = inject(DestroyRef);
+
+  ngOnInit() {
+    const subscription = this.httpClient
+      .get<{
+        places: Place[];
+      }>('http://localhost:3000/places', { observe: 'events' })
+      .subscribe({
+        next: (event) => {
+          console.log(event);
+          // console.log(response.body?.places);
+        },
+      });
+
+    this.destroyDef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
+}
